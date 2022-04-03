68be667c143bf4d0b2125e4e80197c5b4bd6997a
Shogun570 <78151301+Shogun570@users.noreply.github.com>
2022-04-03 17:33:31 +0530

    Add files via upload

---
+++ autotype.py
@@ -0,0 +1,19 @@
+
+import time
+import os
+word = "0"
+delay = (input("Enter Delay between each character in ms"))/1000
+while 2<3: 
+ word = input("Enter the words \n")
+ if word == "0":
+     quit()
+ time.sleep(3)
+ auto.typewrite(word, interval = delay)
+ os.system('clear')
+ 
+ 
+ 
+
+ 
+
+	
