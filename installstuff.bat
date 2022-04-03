421a0a4617da5c56da72c468edce47253b364816
Shogun570 <78151301+Shogun570@users.noreply.github.com>
2022-04-03 17:38:14 +0530

    Update installstuff.bat

--- installstuff.bat
+++ installstuff.bat
@@ -1,9 +1,8 @@
 @echo off
 color a
 echo BEFORE TOUCHING ANYTHING HERE, MAKE SURE YOU HAVE PYTHON AND PIP INSTALLED.
-timeout 1
 echo IF U DONT, GO DO IT
-timeout 1
 echo IF U DO, press space
 pause
 pip install pyautogui
+pause
