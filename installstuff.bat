c7b2ffd1b7576f49d2cb819ca767467813342e89
Shogun570 <78151301+Shogun570@users.noreply.github.com>
2022-04-03 17:36:36 +0530

    Create installstuff.bat

---
+++ installstuff.bat
@@ -0,0 +1,9 @@
+@echo off
+color a
+echo BEFORE TOUCHING ANYTHING HERE, MAKE SURE YOU HAVE PYTHON AND PIP INSTALLED.
+timeout 1
+echo IF U DONT, GO DO IT
+timeout 1
+echo IF U DO, press space
+pause
+pip install pyautogui
