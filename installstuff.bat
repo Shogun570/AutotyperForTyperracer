a0618fa5d979777fe4e6a1b4071b94ab195d2e4a
Shogun570 <78151301+Shogun570@users.noreply.github.com>
2022-08-19 18:23:31 +0530

    Delete installstuff.bat

--- installstuff.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-color a
-echo BEFORE TOUCHING ANYTHING HERE, MAKE SURE YOU HAVE PYTHON AND PIP INSTALLED.
-echo IF U DONT, GO DO IT
-echo IF U DO, press space
-pause
-pip install pyautogui
-pause
